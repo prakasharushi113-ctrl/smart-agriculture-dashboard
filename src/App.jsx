449240634dbcd0afd844cc94772376461d2278b6
Arushi <prakasharushi113@gmail.com>
2026-06-12 23:22:41 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1569,13 +1569,13 @@ useEffect(() => {
   const fetchSensorData = async () => {
     try {
       const response = await fetch(
-        "http://localhost:3001/get-sensors"
+      "https://smart-agri-backend-b2td.onrender.com/get-sensors"
       );
 
       const data = await response.json();
       
       const historyResponse = await fetch(
-  "http://localhost:3001/sensor-history"
+ "https://smart-agri-backend-b2td.onrender.com/get-sensors"
 );
 
 const historyData = await historyResponse.json();
